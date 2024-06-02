45865e85be8f0de3f1b472a585220f087d302871
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-02 20:33:24 +0530

    App and Pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,10 @@
 // pages/_app.js
 import React from 'react'
 import "@/styles/globals.css";
-import Layout from '@/app/layout';
 
 const PageLayout = ({ Component, pageProps }) => {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <Component {...pageProps} />
     );
 };
 
