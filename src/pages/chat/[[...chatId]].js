e02aed84a32fb47bef9dd0f754f3618c04a0a81e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-02 21:02:25 +0530

    Changes

--- src/pages/chat/[[...chatId]].js
+++ src/pages/chat/[[...chatId]].js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Head from 'next/head'
-import "@/styles/globals.css";
 import { ChatSidebar } from '@/components/ChatSidebar';
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
