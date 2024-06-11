5a4596690ad9c222d3c12395621507461e4ad5d9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-11 22:33:36 +0530

    Update Message.js

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -10,16 +10,10 @@ export const Message = ({ role, content }) => {
   console.log("USER: ", user);
 
   return (
-    <div className={`grid grid-cols-[30px_1fr] gap-5 p-5 ${role === "assistant"
-      ? "bg-gray-600"
-      : role === "notice"
-        ? "bg-red-600"
-        : ""
-      }`}>
+    <div className={`grid grid-cols-[30px_1fr] gap-5 p-5 ${role === "assistant" ? "bg-gray-600" : role === "notice" ? "bg-red-600" : ""}`}>
       <div>
         {role === "user" && !!user && (
-          <Image src={user?.picture} width={30} height={30} alt="User avatar" className="rounded-sm shadow-md shadow-black/50"
-          />
+          <Image src={user?.picture} width={30} height={30} alt="User avatar" className="rounded-sm shadow-md shadow-black/50" />
         )}
         {role === "assistant" && (
           <div className="flex h-[30px] w-[30px] items-center justify-center rounded-sm bg-gray-800 shadow-md shadow-black/50">
